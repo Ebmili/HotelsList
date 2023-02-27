3f32ccd590a72dc0206458e5514bac7ba4453cd2
Emilia Buczek <101749808+Ebmili@users.noreply.github.com>
2023-02-27 21:56:18 +0100

    ci: added sorting, added select drop-down

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,9 +4,17 @@ import { hotelsCollection } from "../lib/controller";
 import { NewHotelType } from "../types/hotel";
 import Information from "./Information";
 
-function Card() {
+
+function Card(this: any) {
   const [hotels, setHotels] = useState<NewHotelType[]>([]);
   const [search, setSearch] = useState("")
+  const [sortState, setSortState] = useState("none");
+
+  const sortMethods = {
+    none: { method: (a, b) => null },
+    ascending: { method: undefined },
+    descending: { method: (a: number, b: number) => (a > b ? -1 : 1) },
+  };
 
   useEffect(
     () =>
@@ -25,26 +33,41 @@ function Card() {
 
   return (
     <div className="card">
+
+   
+      <select className="select" defaultValue={'DEFAULT'} onChange={(e) => setSortState(e.target.value)}>
+        <option value="DEFAULT" disabled>Filter By</option>
+        <option value="ascending">per Name</option>
+        <option value="descending">per Price</option>
+      </select>
+      <div>
+        {hotels.sort(sortMethods[sortState].method).map((hotel, id) => (
+          <Information key={hotel.id} hotel={hotel}
+          /> 
+        ))}
+      </div>
+
       <div className="search">
-        <input className="search-input" 
+        <input className="inputsearch" 
         value={search} type="text" placeholder="Search for the hotel" onChange={(e) => setSearch(e.target.value)}
         />
     </div>
-    {!!hotels? (
+    {hotels && hotels.length ? (
       <div>
     {hotels?.filter((item:NewHotelType) => {
           if (search === "" && !search.length) {
             return item;
           }
           return item?.title === search;
-        })
+        })        
+        
         ?.map((hotel:NewHotelType) => (<Information key={hotel.id} hotel={hotel}
         /> 
         ))}
         </div>
-    ):(
-      <h2 className="no-hotels">There are no hotels</h2>
-    )}
+    ):
+    <h2 className="no-hotels">There are no hotels</h2>
+}
     </div>
   );
 }
--- src/types/hotel.ts
+++ src/types/hotel.ts
@@ -14,7 +14,6 @@ export interface NewHotelType {
 }
 
 export interface AddHotelType {
-  title: string;
   description: string;
   feature: string;
   location: string;
@@ -24,4 +23,5 @@ export interface AddHotelType {
   review: string;
   totalPrice: string;
   perNight: string;
+  title: string;
 }
