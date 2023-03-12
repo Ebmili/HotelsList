e36213b9853a5e4d8bbbe5eada0538142eb7a316
Emilia Buczek <101749808+Ebmili@users.noreply.github.com>
2023-03-12 22:04:57 +0100

    updated styling, corrected mobile view

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-const Card = React.lazy (() => import('./components/Home'));
-const Create = React.lazy (() => import('./components/Create'));
-const Details = React.lazy (() => import('./components/Details'));
-const NotFound = React.lazy (() => import('./components/NotFound'));
+import Create from "./components/Create";
+import Details from "./components/Details";
+import NotFound from "./components/NotFound";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <Suspense fallback={<p>Loading component...</p>}>
     <Routes>
           <Route path="/create" element={<Create />} />
           <Route path="/hotels/:id" element={<Details />} />
           <Route path="*" element={<NotFound />} />
-          <Route path="/" element={<Card />} />
+          <Route path="/" element={<Home />} />
     </Routes>
-    </Suspense>
   );
 }
 
